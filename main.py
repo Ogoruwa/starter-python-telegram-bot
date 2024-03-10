9e0aabeabe3ebb776fba77b170c2060884150b61
Ogoruwa <fasinaogo12@gmail.com>
2024-03-10 17:33:32 +0100

    Removed debug lines, fixed unbound local variable error

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ webhook_base_url = env.str("WEBHOOK_BASE_URL", False)
 
 app = FastAPI()
 bot = Bot(token=bot_token)
-webhook_info = None
 
 
 class TelegramUpdate(BaseModel):
@@ -25,7 +24,6 @@ class TelegramUpdate(BaseModel):
 
     
 def auth_bot_token(x_telegram_bot_api_secret_token: str = Header(None)) -> str:
-    print( f"\n\nSecret token: {secret_token}\nBot secret token: {x_telegram_bot_api_secret_token}\n" )
     if x_telegram_bot_api_secret_token != secret_token:
         raise HTTPException(status_code=403, detail="Not authenticated")
     return x_telegram_bot_api_secret_token
@@ -36,6 +34,8 @@ async def handle_startup():
     if webhook_base_url:
         webhook_url = webhook_base_url + "/webhook/"
         await bot.set_webhook( url = webhook_url, secret_token = secret_token)
+    webhook_info = await bot.get_webhook_info()
+    print(f"\nWebhook Info: {webhook_info}")
 
 
 @app.get("/health/")
@@ -46,12 +46,9 @@ async def health_check():
 
 @app.post("/webhook/")
 async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_bot_token)):
-    if not webhook_info:
-        webhook_info = await bot.get_webhook_info()
-        print(webhook_info)
     chat_id = update.message["chat"]["id"]
     text = update.message["text"]
-    print("Received message:", update.message)
+    # print("Received message:", update.message)
 
     if text == "/start":
         with open('hello.gif', 'rb') as photo:
