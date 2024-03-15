59fe877ae6e40f2d5cbd30a6e8523c3450a3d9a8
Ogo <156860592+Ogoruwa@users.noreply.github.com>
2024-03-15 10:33:19 +0100

    Trimmed the bot
    Removed unnecessary imports, classes and variables.
    Renamed some variables.
    Added message handler.

--- bot.py
+++ bot.py
@@ -1,33 +1,9 @@
-from typing import Any
-from pydantic import BaseModel
 from telegram import BotCommand, ForceReply
-from telegram.ext import Application, CallbackContext, CommandHandler, MessageHandler, ContextTypes, ExtBot, JobQueue
-
-
-class WebhookUpdate(BaseModel):
-    update_id: int
-    message: dict
-
-
-class BotContext(CallbackContext[ExtBot, dict, dict, dict]):
-    """
-    Custom CallbackContext class that makes `user_data` available for updates of type
-    `WebhookUpdate`.
-    """
-
-    @classmethod
-    def from_update(
-        cls,
-        update: object,
-        application: "Application",
-    ) -> "BotContext":
-        if isinstance(update, WebhookUpdate):
-            return cls(application=application, user_id=update.user_id)
-        return super().from_update(update, application)
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes
 
 
 # Bot methods
-async def set_bot_commands_menu(application: Application[ExtBot[None], Any, Any, Any, Any, JobQueue]) -> None:
+async def set_bot_commands_menu(application: Application) -> None:
     # Register commands for Telegram bot (menu)
     commands = [
         BotCommand( "start", "Start the bot" ),
@@ -40,22 +16,25 @@ async def set_bot_commands_menu(application: Application[ExtBot[None], Any, Any,
         print(f"Can't set commands - {e}")
 
 
-
-async def create_bot_application(bot_token: str, secret_token: str, bot_web_url: str):
+async def create_bot_application(bot_token: str, secret_token: str, bot_web_url: str) -> None:
     """Set up bot application and a web application for handling the incoming requests."""
-    context_types = ContextTypes(context=BotContext)
+    context_types = ContextTypes.DEFAULT_TYPES
 
     # Set updater to None so updates are handled by webhook
     application = Application.builder().token(bot_token).updater(None).context_types(context_types).build()
 
     # Set webhook: url and secret_key
     await application.bot.set_webhook( url = bot_web_url, secret_token = secret_token )
+    await set_bot_commands_menu(application)
 
     # Add handlers here
+    application.add_error_handler(handle_error)
+    
     application.add_handler( CommandHandler("start", cmd_start) )
     application.add_handler( CommandHandler("help", cmd_help) )
     application.add_handler( CommandHandler("ping", cmd_ping) )
-
+    
+    application.add_handler( MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message) )
 
     return application
 
@@ -63,14 +42,20 @@ async def create_bot_application(bot_token: str, secret_token: str, bot_web_url:
 
 # Create handlers here
 
-async def on_error(update: WebhookUpdate, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def handle_error(update: Update, context: ContextTypes.DEFAULT_TYPES) -> None:
     """Handles errors"""
     message = "I have encountered an error"
     print("I have an error")
     await update.message.reply_text(message)
 
 
-async def cmd_start(update: WebhookUpdate, context: ContextTypes.DEFAULT_TYPE):
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPES) -> None:
+    "Handles messages"
+    message = "I'm not in mood to chat"
+    await update.reply_text(message)
+
+
+async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPES) -> None:
     user = update.effective_user
     message = f"""<p>Hi {user.mention_html()}!</p>\n
         <p>I am ARIES, Ogo's botler 😄</p>\n
@@ -79,11 +64,11 @@ async def cmd_start(update: WebhookUpdate, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_html( message, reply_markup = ForceReply(selective=True) )
 
 
-async def cmd_help(update: WebhookUpdate, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def cmd_help(update: Update, context: ContextTypes.DEFAULT_TYPES) -> None:
     await update.message.answer(f"Your ID: {update.from_user.id}")
 
 
-async def cmd_tag(update: WebhookUpdate, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def cmd_tag(update: Update, context: ContextTypes.DEFAULT_TYPES) -> None:
     try:
         await update.send_copy(chat_id=update.chat.id)
     except Exception as e:
@@ -91,9 +76,10 @@ async def cmd_tag(update: WebhookUpdate, context: ContextTypes.DEFAULT_TYPE) -> 
         await update.message.answer("Nice try!")
 
 
-async def cmd_ping(update: WebhookUpdate, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def cmd_ping(update: Update, context: ContextTypes.DEFAULT_TYPES) -> None:
     try:
         await update.message.answer("pong")
     except Exception as e:
         print(f"Can't send message - {e}")
         await update.message.answer("failed")
+
