a924ea9b85fa7817850db39cac5136140f8f8576
Ogo <156860592+Ogoruwa@users.noreply.github.com>
2024-03-16 11:15:38 +0100

    Fixed FastAPI error caused by invalid Pydantic field type
    Created Pydantic base model to use as type for update parameter in webhook function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from environs import Env
 from routes import router
 from telegram import Update
+from pydantic import BaseModel
 from bot import create_bot_application
 from contextlib import asynccontextmanager
-from fastapi import Depends, FastAPI, Header, HTTPException, Response
+from fastapi import Depends, FastAPI, Header, HTTPException, status
 
 
 env = Env()
@@ -14,19 +15,22 @@ bot_web_url = env.str("BOT_WEB_URL")
 secret_token = env.str("SECRET_TOKEN")
 
 
+class WebhookUpdate(BaseModel):
+    update_id: int
+    message: dict
+
+
 def auth_bot_token(x_telegram_bot_api_secret_token: str = Header(None)) -> str:
     if x_telegram_bot_api_secret_token != secret_token:
         raise HTTPException(status_code=403, detail="Not authenticated")
     return x_telegram_bot_api_secret_token
 
 
-@router.post("/webhook/")
-async def webhook(update: Update, token: str = Depends(auth_bot_token)) -> Response:
+@router.post("/webhook/", status_code = status.HTTP_204_NO_CONTENT)
+async def webhook(update: Update, token: str = Depends(auth_bot_token)) -> None:
     """Handle incoming Telegram updates by putting them into the `update_queue`"""
     update = Update.de_json( update, update.bot ) 
     await application.update_queue.put(update)
-    return Response()
-
 
 
 @asynccontextmanager
